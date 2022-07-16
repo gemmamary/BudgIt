454a1d54902c3a2bc32db82cd9fe23d76a187f75
gemmamary <gmaree93x@gmail.com>
2022-07-16 22:23:52 +0100

    Added edit pages and icon to accounts

---
+++ BudgIt/Pages/Accounts/Edit.cshtml.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyBudgetApp.Core;
+using MyBudgetApp.Data;
+
+namespace BudgIt.Pages.Accounts
+{
+    public class EditModel : PageModel
+    {
+        private readonly IAccountData accountData;
+        public Account Account { get; set; }
+
+        public EditModel(IAccountData accountData)
+        {
+            this.accountData = accountData;
+        }
+
+        public IActionResult OnGet(int accountId)
+        {
+            Account = accountData.GetById(accountId);
+
+            if(Account == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
+        }
+    }
+}
