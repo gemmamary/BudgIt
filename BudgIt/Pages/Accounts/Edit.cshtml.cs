68109eff984f425065003042e9ab05cc56c1aba2
gemmamary <gmaree93x@gmail.com>
2022-07-21 22:37:38 +0100

    Created a field for Accounts and added a HTML Helper to the constructor for EditModel. Used a HTML Helper method to get the Account Type enum values so that we can use them in a select list in the Edit form.

--- BudgIt/Pages/Accounts/Edit.cshtml.cs
+++ BudgIt/Pages/Accounts/Edit.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using MyBudgetApp.Core;
 using MyBudgetApp.Data;
 
@@ -12,16 +13,33 @@ namespace BudgIt.Pages.Accounts
     public class EditModel : PageModel
     {
         private readonly IAccountData accountData;
-        public Account Account { get; set; }
+        private readonly IHtmlHelper htmlHelper;
 
-        public EditModel(IAccountData accountData)
+        public Account Account { get; set; }
+        
+        /// <summary>
+        /// When editing an account, we want a dropdown list that displays the options from the Account Type enum. 
+        /// Declaring the Accounts variable here so that we can use it in the form.
+        /// </summary>
+        public IEnumerable<SelectListItem> Accounts { get; set; }
+
+        /// <summary>
+        /// Sets the properties needed for the edit Account operations.
+        /// </summary>
+        /// <param name="accountData">This is used to access the account data fields and methods.</param>
+        /// <param name="htmlHelper">This is used to get the values from the AccountType enum.</param>
+        public EditModel(IAccountData accountData,
+                         IHtmlHelper htmlHelper)
         {
             this.accountData = accountData;
+            this.htmlHelper = htmlHelper;
         }
 
         public IActionResult OnGet(int accountId)
         {
             Account = accountData.GetById(accountId);
+
+            Accounts = htmlHelper.GetEnumSelectList<AccountType>();
 
             if(Account == null)
             {
